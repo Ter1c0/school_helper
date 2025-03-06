1a21dfb384d80d4ad36ecdaac55e190b73070855
Denis <123729906+Ter1c0@users.noreply.github.com>
2025-03-06 13:15:57 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,13 @@
 ## === models.py ===
 SUBJECTS = ["Математика", "Русский", "Физика", "Химия", "История"]
 DAYS = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+CONTENT_TYPES = ["ДЗ", "Демо", "Расписание"]
 
 def get_subjects():
     return SUBJECTS
 
 def get_days():
     return DAYS
+
+def get_content_types():
+    return CONTENT_TYPES
